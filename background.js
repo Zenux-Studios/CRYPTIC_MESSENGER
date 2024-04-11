b1db425fe093c457e97562ddc13b2de06e337cd0
Prithwish Chakraborty <prithwishchakaborty05@gmail.com>
2024-04-11 17:51:15 +0530

    background error fixed

--- background.js
+++ background.js
@@ -2,18 +2,20 @@ const dsec = ["DSEC", "SPENC", "DEC"];
 const set = async () => {
     chrome.storage.local.get("set", (res) => {
         if (res.set) return;
-        else
+        else {
             dsec.forEach((ele) => {
                 chrome.storage.local.set({ [ele]: false });
             });
+            chrome.storage.local.set({ set: true });
+            console.log("byfrost set");
+        }
     });
-
-    console.log("byfrost set");
 };
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message.propagator, "message received");
     if (message.propagator) {
-        console.log(message.propagator);
+        console.log(chrome.storage.local.get(message.propagator));
         chrome.storage.local.get(message.propagator).then(sendResponse);
         return true;
     }
--- other.js
+++ other.js
@@ -1,1 +1,9 @@
-console.log("@DARKLOD SECURED")
+console.log("@DARKLOD SECURED");
+
+const verdict = async (val) => {
+    const state = await chrome.runtime.sendMessage({ propagator: val });
+    confirm.log(state);
+    return state[val];
+};
+
+console.log(verdict("DSEC"), "@DARKLOD SECURED");
