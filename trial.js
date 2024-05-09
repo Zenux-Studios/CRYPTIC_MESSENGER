59b3a486e49cd885b80d0fe5d950e462ac88d76c
LORD PRITHWISH <prithwishchakraborty05@gmail.com>
2024-05-09 21:23:15 +0530

    tidy up

--- trial.js
+++ trial.js
@@ -7,18 +7,3 @@ const decryptionprotocol = async () => {
         console.log(mess)
     });
 };
-
-// let limess = box.querySelectorAll("span:not(:empty):has(a)");
-// if (mess.length == 3) {
-//     decryption(mess[0]);
-// }
-// if (mess.length == 5) {
-//     decryption(mess[1]);
-//     decryption(mess[2]);
-// }
-// if (limess.length === 2 && dlsc) {
-//     limess[1].firstElementChild.textContent = "@DARKLORD SECURED";
-//     if (mess.length > 2) {
-//         mess[0].parentElement.parentElement.parentElement.parentElement.remove();
-// }
-// }
